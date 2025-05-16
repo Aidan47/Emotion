0523b61f8969b7c46cbfe9dbc53453305c629b41
Aidan R <aidanrolle2@gmail.com>
2025-05-16 02:02:21 -0400

    Implemented early stopping | Added a validation set | Normalized training data | Changed 'cuda' to 'mps' for proper gpu

--- train.py
+++ train.py
@@ -12,40 +12,58 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.5], std=[0.5])
 ])
 
-Train   = datasets.ImageFolder(root='archive/train', transform=transform)
-Train_loader = DataLoader(Train, batch_size=32, shuffle=True)
-
-    # shape of data
-#img, label = next(iter(Train_loader))
-#print(img.shape)
-#print(label.shape, end="\n\n")
+FullDS  = datasets.ImageFolder(root='archive/train', transform=transform)
+tSize   = int(0.8 * len(FullDS))    # Training set size
+vSize   = len(FullDS) - tSize       # Validation set size
+Train, Dev = torch.utils.data.random_split(FullDS, [tSize, vSize])
+Train_loader    = DataLoader(Train, batch_size=64, shuffle=True)
+Val_loader      = DataLoader(Train, batch_size=64, shuffle=False)
 
 
     # training
-device      = "cuda" if torch.cuda.is_available() else "cpu"
+device      = "mps" if torch.mps.is_available() else "cpu"
 model       = NN().to(device)
 optimizer   = torch.optim.Adam(model.parameters())
 criterion   = torch.nn.CrossEntropyLoss()
 
-epochs = 5
-AvgLoss = []
-for epoch in range(epochs):
+epoch = 0
+vLoss = float('inf')
+tolerance = 2
+noImprovment = 0
+while noImprovment < tolerance:
     model.train()
     
+    losses = []
     for imgs, labels in Train_loader:
         imgs, labels = imgs.to(device), labels.to(device)
         
         optimizer.zero_grad()
         logits  = model(imgs)
         loss    = criterion(logits, labels)
-        AvgLoss.append(loss.detach().numpy())
+        losses.append(loss.detach().cpu().numpy())
         loss.backward()
         optimizer.step()
         
-    print(f"Epoch: {epoch+1},  Loss: {np.average(AvgLoss[-10:])}")    # info on epoch
+    trainLoss = np.average(losses)  # Training loss
+    epoch += 1
+        
     # early stopping
+    model.eval()
+    losses = []
+    with torch.no_grad():   # Validation Testing
+        for imgs, labels in Val_loader:
+            imgs, labels = imgs.to(device), labels.to(device)
+            logits  = model(imgs)
+            loss    = criterion(logits, labels)
+            losses.append(loss.detach().cpu().numpy())
     
-print()
+    valLoss = np.average(losses)    # Average loss across validation set
 
-torch.save(model.state_dict(), "weights.pth")
-print("done")
+    if valLoss < vLoss:
+        noImprovment = 0
+        vLoss = valLoss
+        torch.save(model.state_dict(), "weights.pth")
+    else:
+        noImprovment += 1
+    
+    print(f"Epoch: {epoch} | Training Loss: {trainLoss:0.4f} | Validation Loss: {valLoss:0.4f}")  # info on epoch
